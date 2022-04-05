1b85f465431477c7fbe0df5e358b5207c1474e48
mona21000 <98873477+mona21000@users.noreply.github.com>
2022-04-05 13:14:23 +0300

    Admin

---
+++ team7/src/main/java/net/codejava/UserDAO.java
@@ -0,0 +1,36 @@
+package net.codejava;
+ 
+import java.sql.*;
+
+import net.codejava.User;
+ 
+public class UserDAO {
+ 
+    public User checkLogin(String email, String password) throws SQLException,
+            ClassNotFoundException {
+        String jdbcURL = "jdbc:mysql://127.0.0.1:3306/?user=root";
+        String dbUser = "root";
+        String dbPassword = "password";
+ 
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+        String sql = "SELECT * FROM users WHERE email = ? and password = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, email);
+        statement.setString(2, password);
+ 
+        ResultSet result = statement.executeQuery();
+ 
+        User user = null;
+ 
+        if (result.next()) {
+            user = new User();
+            user.setFullname(result.getString("fullname"));
+            user.setEmail(email);
+        }
+ 
+        connection.close();
+ 
+        return user;
+    }
+}
