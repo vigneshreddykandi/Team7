afc94918cca2c890cfabc34b37a13710316b19df
VIGNESH REDDY KANDI <91023141+vigneshreddykandi@users.noreply.github.com>
2022-04-07 20:40:50 +0300

    update1

---
+++ team7/src/main/java/candidate/Candidate.java
@@ -0,0 +1,73 @@
+package candidate;
+
+public class Candidate {
+	private int ID;
+	private String surname; 
+	private String firstname;
+	private String party;
+	private String location;
+	private int ika;
+	private String why_commission;
+	private String what_athes;
+	private String professional;
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getParty() {
+		return party;
+	}
+	public void setParty(String party) {
+		this.party = party;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public int getIka() {
+		return ika;
+	}
+	public void setIka(int ika) {
+		this.ika = ika;
+	}
+	public String getWhy_commission() {
+		return why_commission;
+	}
+	public void setWhy_commission(String why_commission) {
+		this.why_commission = why_commission;
+	}
+	public String getWhat_athes() {
+		return what_athes;
+	}
+	public void setWhat_athes(String what_athes) {
+		this.what_athes = what_athes;
+	}
+	public String getProfessional() {
+		return professional;
+	}
+	public void setProfessional(String professional) {
+		this.professional = professional;
+	}
+	
+	
+	
+
+
+}
